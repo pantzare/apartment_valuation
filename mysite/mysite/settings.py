ea7187349a74c62f2dfcaa21bb218201eb207444
Victor Pantzare <victor.pantzare@gmail.com>
2020-12-30 19:50:35 +0100

    urls not working

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'w*gvt$80(!+lb9#wj26(9-%f9ve1c7)^^6a^x113jyoe_si$@s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
